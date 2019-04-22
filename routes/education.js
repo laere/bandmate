dca77004e95dfc5aa614105ef131821258d7a20a
Zack Bostian <bostianz@gmail.com>
2019-04-22 08:30:45 -0400

    education post/delete/put routes done:

--- routes/education.js
+++ routes/education.js
@@ -5,4 +5,68 @@ const validateEducation = require("../validation/educationValidation");
 const errors = require("../errors/errors");
 const Profile = require("../models/Profile");
 
+router.post(
+  "/",
+  myAsync(async (req, res, next) => {
+    const { error } = validateEducation(req.body);
+    if (error) {
+      res.status(400).send(error.details[0].message);
+      next();
+    }
+
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    if (!profile) next(errors.processReq);
+
+    profile.education.push(req.body);
+
+    await profile.save();
+
+    res.send(profile);
+  })
+);
+
+router.delete(
+  "/:educationId",
+  myAsync(async (req, res, next) => {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    if (!profile) next(errors.processReq);
+
+    let education = profile.education.id(req.params.educationId);
+
+    if (!education) next(errors.processReq);
+
+    education.remove();
+
+    await profile.save();
+
+    res.send(profile);
+  })
+);
+
+router.put(
+  "/:educationId",
+  myAsync(async (req, res, next) => {
+    let profile = await Profile.findOneAndUpdate(
+      {
+        user: req.user.id,
+        "education._id": req.params.educationId
+      },
+      {
+        $set: {
+          "education.$": req.body
+        }
+      },
+      { new: true }
+    );
+
+    if (!profile) next(errors.processReq);
+
+    await profile.save();
+
+    res.send(profile);
+  })
+);
+
 module.exports = router;
